8831def05fd5cbc4a6123ec55b12ccd8d1c37bd5
Fightbuster2 <Rune.cuijpers@sintlucasedu.nl>
2023-10-31 12:40:18 +0100

    Initial push
    first push

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,49 @@
-
 let img1 = document.getElementById("pong");
+let img2 = document.getElementById("threadwear");
+let img3 = document.getElementById("webshop");
 let backdrop = document.getElementById("backdrop");
 let modal1 = document.getElementById("modal1");
+let modal2 = document.getElementById("modal2");
+let modal3 = document.getElementById("modal3");
 
-img1.onclick = showModal1;
+img1.onclick = (event) => {
+    event.preventDefault(); // Prevent the default behavior of the anchor tag
+    showModal1();
+}
 
-backdrop.onclick = hideModal;
+img2.onclick = (event) => {
+    event.preventDefault();
+    showModal2();
+}
 
-function hideModal(){
+img3.onclick = (event) => {
+    event.preventDefault();
+    showModal3();
+}
+
+
+function hideModal() {
     backdrop.style.display = "none";
     modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
 }
 
-function showModal1(){
-    modal1.style.display = "block";
+function showModal1() {
     backdrop.style.display = "block";
+    modal1.style.display = "block";
 }
-hideModal()
+
+function showModal2() {
+    backdrop.style.display = "block";
+    modal2.style.display = "block";
+}
+
+function showModal3() {
+    backdrop.style.display = "block";
+    modal3.style.display = "block";
+}
+
+backdrop.onclick = hideModal;
+hideModal();
+
